ead1650a50e0d58e89d904641816577d3d67844e
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-03-28 15:33:16 +0900

    center align and dropdown align

--- comparing_pokemon_moves_1.py
+++ comparing_pokemon_moves_1.py
@@ -4,30 +4,44 @@ import dash_core_components as dcc
 import base64
 from os import listdir
 from os.path import isfile, join
+import pandas as pd
 MYPATH = "pokemon.json-master/thumbnails"
 
+pokedex_data = pd.read_csv("all_pokemon_names_types.csv", index_col=0)
 
 app = dash.Dash()
 
-list_of_images = [MYPATH + "/" + f for f in listdir(MYPATH) if isfile(join(MYPATH, f))]
+list_of_images =\
+    [MYPATH + "/" + f for f in listdir(MYPATH) if isfile(join(MYPATH, f))]
+list_of_images.sort()
 
 app.layout = html.Div([
-    dcc.Dropdown(
-        id='image-dropdown-1',
-        style={'width': '50%'},
-        options=[{'label': i, 'value': i} for i in list_of_images],
-        # initially display the first entry in the list
-        value=list_of_images[0]
-    ),
-    dcc.Dropdown(
-        id='image-dropdown-2',
-        style={'width': '50%'},
-        options=[{'label': i, 'value': i} for i in list_of_images],
-        # initially display the first entry in the list
-        value=list_of_images[0]
-    ),
-    html.Img(id='image-1'),
-    html.Img(id='image-2')
+    html.Div([
+        dcc.Dropdown(
+            id='image-dropdown-1',
+            options=[{'label': pokedex_data[pokedex_data["id"] == num + 1]["english_name"].values[0],
+                    'value': i} for num, i in enumerate(list_of_images)],
+            # initially display the first entry in the list
+            value=list_of_images[0]
+        ),
+    ], style={'width': '49%', 'display': 'inline-block'}),
+    html.Div([
+        dcc.Dropdown(
+            id='image-dropdown-2',
+            options=[{'label': pokedex_data[pokedex_data["id"] == num + 1]["english_name"].values[0],
+                    'value': i} for num, i in enumerate(list_of_images)],
+            # initially display the first entry in the list
+            value=list_of_images[0]
+        ),
+    ], style={'width': '49%', 'display': 'inline-block'}),
+    html.Div([
+        html.Div([
+            html.Img(id='image-1'),
+        ], style={'width': '49%', 'display': 'inline-block', 'margin-left': 'auto', 'margin-right': 'auto'}),
+        html.Div([
+            html.Img(id='image-2')
+        ], style={'width': '49%', 'display': 'inline-block', 'margin-left': 'auto', 'margin-right': 'auto'}),
+    ], style={'text-align': 'center'}),
 ])
 
 
